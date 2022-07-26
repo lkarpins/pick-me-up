f8c466ca95a225e83731c821c2fa48c00329924c
Lourdes Benites <lourdesben95@gmail.com>
2022-07-26 10:26:33 -0600

    Refcatored to Routes component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import { getCompliments, getAdvice } from "./ApiCalls";
 import "./App.css";
 import { Button } from "./components/Button/Button";
-import { AppContainer } from "./components/AppContainer/AppContainer";
-import { ComplimentContainer } from "./components/ComplimentContainer/ComplimentContainer";
-import { AdviceContainer } from "./components/AdviceContainer/AdviceContainer";
-import { Route, Switch } from 'react-router-dom';
+import { Routes } from "./components/Routes/Routes";
+
+
 
 
 interface Props { }
@@ -39,17 +38,7 @@ class App extends Component<Props, State> {
           <h1 className="logo">PickMeUp</h1>
           <Button label="See Favorites" />
         </nav>
-        <Switch>
-          <Route exact path="/">
-            <AppContainer />
-          </Route>
-          <Route exact path="/compliment">
-            <ComplimentContainer compliment={this.state.compliment} />
-          </Route>
-          <Route exact path="/advice">
-            <AdviceContainer advice={this.state.advice} />
-          </Route>
-        </Switch>
+        <Routes compliment={this.state.compliment} advice={this.state.advice} />
       </main>
     );
   }
---
+++ src/components/Routes/Routes.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { AdviceContainer } from '../AdviceContainer/AdviceContainer';
+import { AppContainer } from '../AppContainer/AppContainer';
+import { ComplimentContainer } from '../ComplimentContainer/ComplimentContainer';
+
+export interface RoutesProps {
+    compliment: string;
+    advice: string;
+}
+
+export const Routes = ({
+    compliment,
+    advice
+}: RoutesProps) => {
+    return (
+        <Switch>
+        <Route exact path="/">
+          <AppContainer />
+        </Route>
+        <Route exact path="/compliment">
+          <ComplimentContainer compliment={compliment} />
+        </Route>
+        <Route exact path="/advice">
+          <AdviceContainer advice={advice} />
+        </Route>
+      </Switch>
+    );
+}
