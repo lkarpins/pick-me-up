c41ab266992d8f5de58559225108256e21ee19d9
Rachel Allen <rallenco13@gmail.com>
2022-07-30 11:00:18 -0600

    conditional rendering of favorites button and no favorites

--- src/components/FavoritesContainer/FavoritesContainer.tsx
+++ src/components/FavoritesContainer/FavoritesContainer.tsx
@@ -14,24 +14,31 @@ export const FavoritesContainer = ({
   favoriteSelection,
   toggleFavorites,
 }: FavoritesContainerProps) => {
-
-  const favoritesContent = favoriteSelection.map((favSelection: string) => {
-    return (
-      <div className="favorite-item" key="favSelection">
-        <span
-          className="material-icons icon"
-          onClick={() => toggleFavorites(selection, favSelection)}
-        >
-          favorite
-        </span>
-        <p className="favorite-text">{favSelection}</p>
-      </div>
-    );
-  });
+  const favoritesContent = favoriteSelection.map(
+    (favSelection: string, index) => {
+      return (
+        <div className="favorite-item" key={index}>
+          <span
+            className="material-icons icon"
+            onClick={() => toggleFavorites(selection, favSelection)}
+          >
+            favorite
+          </span>
+          <p className="favorite-text">{favSelection}</p>
+        </div>
+      );
+    }
+  );
   return (
     <div className={`favorites ${selection}-section`}>
       <h2 className="favorites-heading">{selectionHeading}</h2>
-      <div className="favorites-content">{favoritesContent}</div>
+      <div className="favorites-content">
+        {favoriteSelection.length > 0 ? (
+          favoritesContent
+        ) : (
+          <p className="favorite-text">No Favorites Saved</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Button } from "../Button/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navigation.css";
 
 export interface NavigationProps {
@@ -8,14 +8,18 @@ export interface NavigationProps {
 }
 
 export const Navigation = ({ label }: NavigationProps) => {
+  const location = useLocation()
+  console.log("location", location);
+  
   return (
     <nav>
       <Link to="/" className="link-style">
         <h1 className="logo">PickMeUp</h1>
       </Link>
-      <Link to="/favorites">
+      {location.pathname !== "/favorites" && <Link to="/favorites">
         <Button label="See Favorites" />
-      </Link>
+      </Link>}
+      
     </nav>
   );
 };
