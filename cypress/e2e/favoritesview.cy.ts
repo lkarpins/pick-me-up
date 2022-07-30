dd81db16b8f19fb3a1f279533420a3ddea2fee9b
Rachel Allen <rallenco13@gmail.com>
2022-07-30 17:19:37 -0600

    test passing for no favorites in local storage

--- cypress/e2e/favoritesview.cy.ts
+++ cypress/e2e/favoritesview.cy.ts
@@ -1,6 +1,18 @@
-describe('empty spec', () => {
-  
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("PickMeUp FavoriesView", () => {
+  describe("Initial View", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/favorites");
+    });
+
+    it("should load with no saved favorites", () => {
+      cy.window()
+        .its("localStorage")
+        .invoke("getItem", "complimentFavorites")
+        .should("not.exist");
+      cy.window()
+        .its("localStorage")
+        .invoke("getItem", "adviceFavorites")
+        .should("not.exist");
+    });
+  });
+});
